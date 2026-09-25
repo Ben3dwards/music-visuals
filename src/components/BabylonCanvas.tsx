295176dffa24eff132d259b40b1b7673f3685d2b
Ben3dwards <113576510+Ben3dwards@users.noreply.github.com>
2026-09-25 02:18:28 +0100

    Cube spins, changed its colour

--- src/components/BabylonCanvas.tsx
+++ src/components/BabylonCanvas.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import{Engine, Scene, ArcRotateCamera, HemisphericLight, MeshBuilder, Vector3} from "@babylonjs/core"
+import{Engine, Scene, ArcRotateCamera, HemisphericLight, MeshBuilder, Vector3, StandardMaterial, Color3 } from "@babylonjs/core"
 
 export default function BabylonCanvas() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -16,9 +16,17 @@ export default function BabylonCanvas() {
 
         new HemisphericLight("light", new Vector3 (0, 1, 0), scene);
 
-        MeshBuilder.CreateBox("cube", {size: 1}, scene);
+        const cube = MeshBuilder.CreateBox("cube", {size: 1}, scene);
 
-        engine.runRenderLoop(() => scene.render());
+        const material = new StandardMaterial ("cubeMaterial", scene);
+        material.diffuseColor = new Color3(1, 0, 0.5);
+        cube.material = material;
+
+        engine.runRenderLoop(() => { 
+            cube.rotation.y += 0.01
+            cube.rotation.x += 0.01
+            scene.render()
+        });
 
         const onResize = () => engine.resize();
         window.addEventListener("resize", onResize);
